a1e0ede924e45e43c44049056caa95bee03cbf5e
nerzula <alexzloid@gmail.com>
2021-03-24 08:16:05 +0300

    create adaptib two blocks

--- webovio/js/app.js
+++ webovio/js/app.js
@@ -45,5 +45,22 @@ let slider = new Swiper('.company__swiper', {
         stopOnLastSlide: false,
         disableOnInteraction: false,
         autoHeight: false
+    },
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+        // when window width is >= 480px
+        480: {
+            slidesPerView: 3,
+            spaceBetween: 30
+        },
+        // when window width is >= 640px
+        640: {
+            slidesPerView: 4,
+            spaceBetween: 40
+        }
     }
 });
